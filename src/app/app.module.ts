c6a27ee36e1cdbd69810d26eeb958c758d43d9e1
Suchron Rizal <suchronrizal@gmail.com>
2020-06-26 00:08:31 +0700

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -9,7 +11,13 @@ import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AboutComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    environment.production
+      ? ServiceWorkerModule.register('../ngsw-config.json')
+      : []
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
